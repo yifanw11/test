d06af995a2128658e7f8a2fca11c737540c64703
Yifan <yifanw11@student.unimelb.edu.au>
2023-05-16 16:45:23 +1000

    Add files via upload

---
+++ NORDIC Run/update_dockers.sh
@@ -0,0 +1,6 @@
+sudo docker pull karan7docker/nbn_repo:base
+sudo docker pull karan7docker/nbn_repo:load
+sudo docker pull karan7docker/nbn_repo:dev
+sudo docker pull karan7docker/nbn_repo:read_counters
+sudo docker pull karan7docker/nbn_repo:flask
+sudo docker pull karan7docker/nbn_repo:react
